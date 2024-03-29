e67cce3a44dc24f178faf2f1a83d3b99b1fabdf7
yixiaojiu <yi_xiao_jiu@qq.com>
2024-03-29 12:52:54 +0800

    feat: :tada: initialize unocss

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "@tarojs/components";
+import "./index.less";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className='text-sky text-sm'>aaaa</Text>
+    </View>
+  );
+}
